6c6592ed7f91813cf62bbc50cb2b7d5fcdfeb9ff
Lungelo Mavundla <lungelo@example.com>
2025-07-20 13:18:00 +0200

    created the js file and entered the user prompts for tittle and description in the first title

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // Prompt the user to enter task 1 title
-const title1 = prompt("Enter Task 1 title:"); // 👈 Ask for title input for Task 1
+const title1 = prompt("Enter Task 1 title:");
 // Prompt the user to enter task 1 description
-const description1 = prompt("Enter Task 1 description:"); // 👈 Ask for description input for Task 1
+const description1 = prompt("Enter Task 1 description:");
