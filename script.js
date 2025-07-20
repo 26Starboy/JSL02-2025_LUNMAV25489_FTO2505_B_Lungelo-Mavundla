df8f56cd1a2319746aa0483ab0fdb410e0d4b744
Lungelo Mavundla <lungelo@example.com>
2025-07-20 12:59:53 +0200

    Add basic task input prompts for title and description

---
+++ script.js
@@ -0,0 +1,4 @@
+// Prompt the user to enter task 1 title
+const title1 = prompt("Enter Task 1 title:"); // 👈 Ask for title input for Task 1
+// Prompt the user to enter task 1 description
+const description1 = prompt("Enter Task 1 description:"); // 👈 Ask for description input for Task 1
